fd09dc8eed5311f8be1c8981438824ec28f22041
bigrocs <bigrocs@qq.com>
2019-05-10 12:01:12 +0800

    自动提交 git 代码

--- client/client.go
+++ client/client.go
@@ -3,13 +3,8 @@ package client
 import (
 	"os"
 
-	"github.com/micro/go-micro/broker"
+	_ "github.com/gomsa/tools/client/k8s"
 	"github.com/micro/go-micro/client"
-	"github.com/micro/go-micro/cmd"
-	"github.com/micro/go-micro/server"
-	bkr "github.com/micro/go-plugins/broker/grpc"
-	cli "github.com/micro/go-plugins/client/grpc"
-	srv "github.com/micro/go-plugins/server/grpc"
 
 	authPB "github.com/gomsa/user-srv/proto/auth"
 	userPB "github.com/gomsa/user-srv/proto/user"
@@ -24,14 +19,6 @@ var (
 
 func init() {
 	userSrvName := os.Getenv("USER_SRV_NAME")
-	// // set values for registry/selector
-	// os.Setenv("MICRO_REGISTRY", "kubernetes")
-	// os.Setenv("MICRO_SELECTOR", "static")
-	// setup broker/client/server
-	broker.DefaultBroker = bkr.NewBroker()
-	client.DefaultClient = cli.NewClient()
-	server.DefaultServer = srv.NewServer()
-	cmd.Init()
 
 	User = userPB.NewUsersClient(userSrvName, client.DefaultClient)
 	Auth = authPB.NewAuthClient(userSrvName, client.DefaultClient)
--- go.mod
+++ go.mod
@@ -3,12 +3,10 @@ module github.com/gomsa/user-api
 go 1.12
 
 require (
-	github.com/go-ini/ini v1.41.0 // indirect
 	github.com/golang/protobuf v1.3.1
 	github.com/gomsa/user-srv v0.0.0-20190508091431-9a4b58ee6d65
 	github.com/micro/go-log v0.1.0
 	github.com/micro/go-micro v1.1.0
-	github.com/micro/go-plugins v0.25.0
 	github.com/micro/kubernetes v0.7.0
-	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c
+	golang.org/x/net v0.0.0-20190509222800-a4d6f7feada5
 )
