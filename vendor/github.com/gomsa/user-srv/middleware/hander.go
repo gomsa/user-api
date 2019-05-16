5026cfc042667f70bf445f52a866a2ace32cf3ec
bigrocs <bigrocs@qq.com>
2019-05-16 11:45:49 +0800

    自动提交 git 代码

--- vendor/github.com/gomsa/user-srv/middleware/hander.go
+++ vendor/github.com/gomsa/user-srv/middleware/hander.go
@@ -31,7 +31,7 @@ func (h *Handler) Wrapper(fn server.HandlerFunc) server.HandlerFunc {
 				if !ok {
 					return errors.New("no auth meta-data found in request")
 				}
-				if _, ok := meta["authorization"]; ok {
+				if err, ok := meta["authorization"]; ok {
 					// Note this is now uppercase (not entirely sure why this is...)
 					token := strings.Split(meta["authorization"], "Bearer ")[1]
 					// Auth here
@@ -43,8 +43,8 @@ func (h *Handler) Wrapper(fn server.HandlerFunc) server.HandlerFunc {
 					if err != nil || authResp.Valid == false {
 						return err
 					}
-				}else{
-					return errors.New('Empty authorization')
+				} else {
+					return err
 				}
 			}
 		}
