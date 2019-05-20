b0e1e92567916c7b4e9e37f1d94433bdce588673
bigrocs <bigrocs@qq.com>
2019-05-21 07:45:59 +0800

    自动提交 git 代码

--- hander/auth.go
+++ hander/auth.go
@@ -32,6 +32,17 @@ func (srv *Auth) Auth(ctx context.Context, req *pb.User, res *pb.Token) (err err
 	return err
 }
 
+// Logout 登录退出
+// 后期开发
+// token 增加前期认证根据根据存储的 token 进行认证 和过期查询
+// 通过删除 存储 token 来登出
+// 通过存储多种类型的 token 来实现多端登录
+// token id type type=登录类型
+// 过期时间默认还是在 jwt token 中存储
+func (srv *Auth) Logout(ctx context.Context, req *pb.User, res *pb.Token) (err error) {
+	return err
+}
+
 // ValidateToken 验证 token
 // 并且验证 token 所属用户相关权限
 func (srv *Auth) ValidateToken(ctx context.Context, req *pb.Request, res *pb.Token) (err error) {
