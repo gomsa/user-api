ed7c29172ac991219778a46f9141649d4c86cb20
bigrocs <bigrocs@qq.com>
2019-05-21 11:12:44 +0800

    自动提交 git 代码

--- hander/auth.go
+++ hander/auth.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"errors"
 
-	"github.com/micro/go-log"
-
 	"github.com/gomsa/tools/uitl"
 	pb "github.com/gomsa/user-api/proto/auth"
 	"github.com/gomsa/user-srv/client"
@@ -25,6 +23,9 @@ func (srv *Auth) Auth(ctx context.Context, req *pb.User, res *pb.Token) (err err
 		return err
 	}
 	authRes, err := client.Auth.Auth(context.TODO(), user)
+	if err.Error() == "record not found" {
+		return errors.New("用户名不存在")
+	}
 	if err.Error() == "record not found" {
 		return errors.New("用户名不存在")
 	}
