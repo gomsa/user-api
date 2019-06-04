e03daac40a364c7834d5ab94757d12c968f3b9b9
bigrocs <bigrocs@qq.com>
2019-06-04 17:37:27 +0800

    自动提交 git 代码

--- main.go
+++ main.go
@@ -15,18 +15,21 @@ import (
 )
 
 func main() {
-	Pata := []map[string]interface{}{
-		{"service": "Users", "method": "Create", "auth": false, "policy": false, "display_name": "创建用户", "description": "创建新用户权限。"},
-		{"service": "Users", "method": "Exist", "auth": false, "policy": false, "display_name": "检测用户", "description": "检测用户是否存在权限。"},
-		{"service": "Users", "method": "Get", "auth": true, "policy": false, "display_name": "用户查询", "description": "查询用户信息权限。"},
-		{"service": "Auth", "method": "Auth", "auth": false, "policy": false, "display_name": "用户授权", "description": "用户登录授权返回 token 权限。"},
-		{"service": "Auth", "method": "ValidateToken", "auth": false, "policy": false, "display_name": "权限认证", "description": "权限相关认证权限。"},
+	p_data := []map[string]interface{}{
+		{"service":"user-api",	"method":"Auth.Auth",			"auth":false,	"policy":false,	"display_name": "用户授权",		"description": "用户登录授权返回 token 权限。"},
+		{"service":"user-api",	"method":"Auth.ValidateToken",	"auth":false,	"policy":false,	"display_name": "权限认证",		"description": "权限相关认证权限。"},
+		{"service":"user-api",	"method":"Users.Exist",			"auth":false,	"policy":false,	"display_name": "检测用户",		"description": "检测用户是否存在权限。"},
+		{"service":"user-api",	"method":"Users.Info",			"auth":true,	"policy":false,	"display_name": "检测用户",		"description": "检测用户是否存在权限。"},
+		{"service":"user-api",	"method":"Users.Get",			"auth":true,	"policy":true,	"display_name": "用户查询",		"description": "查询用户信息权限。"},
+		{"service":"user-api",	"method":"Users.Create",		"auth":false,	"policy":false,	"display_name": "创建用户",		"description": "创建新用户权限。"},
+		{"service":"user-api",	"method":"Users.Update",		"auth":true,	"policy":true,	"display_name": "更新用户",		"description": "更新用户信息。"},
+		{"service":"user-api",	"method":"Users.Delete",		"auth":true,	"policy":true,	"display_name": "删除用户",		"description": "删除用户。"},
 	}
 	// 设置权限
 	h := m.Handler{
-		Permissions: Pata
+		Permissions: p_data
 	}
-	srv := k8s.NewService(
+	srv := k8s.New"service"(
 		micro.Name("user-api"),
 		micro.Version("latest"),
 		micro.WrapHandler(h.Wrapper), //验证权限
