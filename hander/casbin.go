c0e0edaaee23af380aaa2aab7cc704d0dbd79f91
bigrocs <bigrocs@qq.com>
2019-07-12 18:33:24 +0800

    自动提交 git 代码

--- hander/casbin.go
+++ hander/casbin.go
@@ -7,7 +7,6 @@ import (
 	pb "github.com/gomsa/user-api/proto/casbin"
 	"github.com/gomsa/user/client"
 	casbinPB "github.com/gomsa/user/proto/casbin"
-	"github.com/micro/go-micro/util/log"
 )
 
 // Casbin 授权服务处理
@@ -129,8 +128,6 @@ func (srv *Casbin) UpdateRoles(ctx context.Context, req *pb.Request, res *pb.Res
 	if err != nil {
 		return err
 	}
-	log.Log(req)
-	log.Log(reqCasbin)
 	resCasbin, err := client.Casbin.UpdateRoles(ctx, reqCasbin)
 	if err != nil {
 		return err
