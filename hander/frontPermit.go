3aa27ec2ffb7c4f38f8203b2ea4ad145fef2ff93
bigrocs <bigrocs@qq.com>
2019-10-05 15:57:54 +0800

    自动提交 git 代码

--- hander/frontPermit.go
+++ hander/frontPermit.go
@@ -5,8 +5,8 @@ import (
 
 	client "github.com/gomsa/tools/k8s/client"
 
-	pb "github.com/gomsa/user-api/proto/frontPermit"
 	casbinPB "github.com/gomsa/user-api/proto/casbin"
+	pb "github.com/gomsa/user-api/proto/frontPermit"
 )
 
 // FrontPermit 权限结构
@@ -17,7 +17,7 @@ type FrontPermit struct {
 // UpdateOrCreate 最高权限同步前端权限列表
 func (srv *FrontPermit) UpdateOrCreate(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
 	// 前端权限写入数据库
-	err = client.Call(ctx, srv.ServiceName, "FrontPermit.UpdateOrCreate", req, res)
+	err = client.Call(ctx, srv.ServiceName, "FrontPermits.UpdateOrCreate", req, res)
 	if err != nil {
 		return err
 	}
@@ -43,32 +43,32 @@ func (srv *FrontPermit) UpdateOrCreate(ctx context.Context, req *pb.Request, res
 
 // All 权限列表
 func (srv *FrontPermit) All(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	return client.Call(ctx, srv.ServiceName, "FrontPermit.All", req, res)
+	return client.Call(ctx, srv.ServiceName, "FrontPermits.All", req, res)
 }
 
 // List 权限列表
 func (srv *FrontPermit) List(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	return client.Call(ctx, srv.ServiceName, "FrontPermit.List", req, res)
+	return client.Call(ctx, srv.ServiceName, "FrontPermits.List", req, res)
 }
 
 // Get 获取权限
 func (srv *FrontPermit) Get(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	return client.Call(ctx, srv.ServiceName, "FrontPermit.Get", req, res)
+	return client.Call(ctx, srv.ServiceName, "FrontPermits.Get", req, res)
 }
 
 // Create 创建权限
 func (srv *FrontPermit) Create(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	return client.Call(ctx, srv.ServiceName, "FrontPermit.Create", req, res)
+	return client.Call(ctx, srv.ServiceName, "FrontPermits.Create", req, res)
 }
 
 // Update 更新权限
 func (srv *FrontPermit) Update(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	return client.Call(ctx, srv.ServiceName, "FrontPermit.Update", req, res)
+	return client.Call(ctx, srv.ServiceName, "FrontPermits.Update", req, res)
 }
 
 // Delete 删除权限
 func (srv *FrontPermit) Delete(ctx context.Context, req *pb.Request, res *pb.Response) (err error) {
-	return client.Call(ctx, srv.ServiceName, "FrontPermit.Delete", &pb.FrontPermit{
+	return client.Call(ctx, srv.ServiceName, "FrontPermits.Delete", &pb.FrontPermit{
 		Id: req.FrontPermit.Id,
 	}, res)
 }
