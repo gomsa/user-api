b8b19832ebfa61e3118e258753d27e5fad1acbf5
bigrocs <bigrocs@qq.com>
2019-10-05 14:56:55 +0800

    自动提交 git 代码

--- hander/user.go
+++ hander/user.go
@@ -139,7 +139,7 @@ func (srv *User) Create(ctx context.Context, req *pb.Request, res *pb.Response) 
 	err = client.Call(ctx, srv.ServiceName, "User.Create", req, res)
 	log.Log("aaa3", req, res)
 	res.User.Password = "123a"
-	log.Log("aaa4", req, res)
+	log.Log("aaa4", srv.ServiceName, req, res)
 	return err
 }
 
